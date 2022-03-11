9125a95cbf43539131f27062e7ad42efa4b9a449
ANUSHKALA <anushkakaimal2003@gmail.com>
2022-03-11 19:13:11 +0530

    app.js

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-
 const app = express();
-//app.use(bodyParser,urlencoded({extended : true}));
 
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 app.use(express.static("public"));
 
 app.get("/",function (req,res) {
 
     res.sendFile(__dirname+"/HTML/signup.html")
     
+})
+
+app.post("/",function (req,res) {
+    
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+
+    console.log(firstName);
+    console.log(lastName);
+    console.log(email);
+
 })
 
 app.listen(3000,function(){
