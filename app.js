f9ed70193a08c61c271b0075eaad95cb1beefef7
ANUSHKALA <anushkakaimal2003@gmail.com>
2022-03-11 14:53:04 +0530

    files

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { urlencoded } = require("body-parser");
 const request = require("request");
 
 const app = express();
-app.use(bodyParser,urlencoded({extended : true}));
+//app.use(bodyParser,urlencoded({extended : true}));
+
+app.use(express.static("public"));
+
+app.get("/",function (req,res) {
+
+    res.sendFile(__dirname+"/HTML/signup.html")
+    
+})
 
 app.listen(3000,function(){
     console.log("This server is active on port 3000.")
