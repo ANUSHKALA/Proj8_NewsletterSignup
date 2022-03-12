6b3dbc28cb5bf396bb650cad7ec0388047d7191e
ANUSHKALA <anushkakaimal2003@gmail.com>
2022-03-12 20:43:38 +0530

    site working

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-// mailchimp credentials
-API = "60c6a28576a4fc40bba8b48380b2330b-us14";
-const aud_key = "27b317e5ad";
-
-/*The process. env property is an inbuilt
- application programming interface of the process module
-  which is used to get the user environment.
- Syntax: process.env. Return Value: This property returns an object containing the user environment 
- */
-
-const mailChimp = require("@mailchimp/mailchimp_marketing")
+const mailChimp = require("@mailchimp/mailchimp_marketing");
 const express = require("express");
 const bodyParser = require("body-parser");
 // const fetch = require("node-fetch");
@@ -31,39 +21,40 @@ app.get("/",function (req,res) {
     res.sendFile(__dirname+"/HTML/signup.html")
 });
 
+mailChimp.setConfig({
+    apiKey : "60c6a28576a4fc40bba8b48380b2330b-us14",
+    server : 'us14',
+});
+
 app.post("/post",(req,res) => {
    
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
+    const listId = "27b317e5ad";
 
-    const data = {
-        members : [
-            {
-                email_address : email,
-                status : "subscribed",
-                merge_fields: {
-                    FNAME : firstName,
-                    LNAME : lastName
-                }
+    const user = {
+        firstName : firstName,
+        lastName : lastName,
+        email : email,
+    };
+
+    async function addContact(){
+
+        const response = await mailChimp.lists.addListMember(listId, {
+            email_address : user.email,
+            status : 'subscribed',
+            merge_fields : {
+                FNAME : user.firstName,
+                LNAME :user.lastName,
             }
-        ]
-    }
+        });  
+        res.sendFile(__dirname+"/HTML/success.html");
+        console.log("FINALLY, it worked!" )
+    } 
 
-    const jsonData = JSON.stringify(data);
+    addContact().catch(err => console.log(err));
 
-    fetch('https://us14.api.mailchimp.com/3.0/lists/27b317e5ad',{
-        method : 'POST',
-        headers : {
-            Authorization: 'auth 60c6a28576a4fc40bba8b48380b2330b-us14'
-        },
-        body: jsonData,
-    })//.then(res.statusCode === 200 ?
-    //     res.redirect( __dirname +'/success.html'):
-    //     res.redirect('/HTML/faliure.html'))
-    //     .catch(err => console.log("not again"))
-
- 
 })
 
 app.listen(3000,function(){
@@ -74,5 +65,6 @@ app.listen(3000,function(){
 
 // API Key
 // 60c6a28576a4fc40bba8b48380b2330b-us14
+
 // Audience key/List ID
 // 27b317e5ad
